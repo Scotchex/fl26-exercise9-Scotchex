edf6559eff9678f6e4fab3b6b5660fb598af56cf
2b2b2b <59747023+Scotchex@users.noreply.github.com>
2026-09-23 13:54:37 -0400

    Done with the header file

--- complex_widget.hpp
+++ complex_widget.hpp
@@ -4,5 +4,36 @@
 #ifndef COMPLEX_WIDGET_H
 #define COMPLEX_WIDGET_H
 
+#include <QWidget>
+
+class QLineEdit;
+class QLabel;
+class QPushButton;
+
+class ComplexWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    // this is the top level widget so no need for a parent
+    explicit ComplexWidget(QWidget *parent = nullptr);
+
+    double getRealPart();
+    double getImaginaryPart();
+    double getMagnitude();
+    double getPhase();
+
+private slots:
+    void updateDisplay();
+
+private:
+    QLineEdit *realPart;
+    QLineEdit *imaginaryPart;
+
+    QLabel *showMagnitude;
+    QLabel *showPhase;
+
+    QPushButton *updateButton;
+};
 
 #endif
